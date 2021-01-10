bad303a1c0ecab1ddf0e75c29663ad944d5edfd9
CassBut <clhurlbut@gmail.com>
2021-01-10 12:22:54 -0800

    inital structure in server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+// dependencies
+const express = require("express");
+
+// express server
+const app = express();
+
+// setting PORT for listener
+const PORT = process.env.PORT || 3001;
+
+// middleware to parse data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// API routes
+
+// listener 
+app.listen(PORT, function () {
+    console.log("The App is listening on PORT: " + PORT);
+});

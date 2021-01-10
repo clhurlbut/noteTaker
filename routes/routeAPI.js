efbee2074747dd527a1660fdf31e8dc4ced52ab1
CassBut <clhurlbut@gmail.com>
2021-01-10 12:49:05 -0800

    inital code for API and HTML routes

---
+++ routes/routeAPI.js
@@ -0,0 +1,26 @@
+//dependencies 
+
+const fs = require('fs');
+
+
+module.exports = function (app) {
+    // get request
+    app.get('/api/notes', function (req, res) {
+        fs.readFile('./db/db.json', (err, data) => {
+            if (err) throw err;
+            newData = JSON.parse(data);
+            res.send(newData);
+        });
+    });
+    // post request 
+    app.post('/api/notes', function (req, res) {
+        const newNotes = req.body;
+        fs.readFile('./db/db.json', (err, data) => {
+            if (err) throw err;
+            newData = JSON.parse(data);
+            newData.push(newNotes);
+        });
+        console.log(newData);
+
+    });
+};
